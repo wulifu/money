91da56444d5dcf08c8e10a76a87a295d401299de
kangning <kangning_it@163.com>
2017-02-13 19:47:56 +0800

    前台框架整合，模板demo

---
+++ admin/frontend/controllers/RebateController.php
@@ -0,0 +1,59 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\db\ActiveRecord;
+use yii\filters\AccessControl;
+use app\models\User;
+use app\models\Finance_project;
+
+/**
+ * 返利模块
+ */
+class RebateController extends Controller
+{
+    public $layout = 'menu';
+
+    /**
+     * 给所有用户返利
+     * @return [type] [description]
+     */
+    public function actionIndex(){
+
+    	ini_set('memory_limit', '250M'); //内存限制
+		set_time_limit(60*10); //执行时间限制
+
+		//取出当前收益利率
+		$rate = 4; //模拟
+		$day_rate = $rate / 365;
+
+    	$userDb = new User();
+        $data = $userDb->find()->where(['>','money','1'])->all();
+        foreach($data as $k => $v){
+            $alter = $v->money*$day_rate;
+            $data[$k]->money = $v->money + $alter;
+            if($data[$k]->save()){
+                $record[] = ['user_id'=>$v->user_id,'money'=>$alter,'time'=>time(),'status'=>2];
+            }else{
+                $error[] = ['user_id'=>$v->user_id];
+            }
+            unset($data[$k]);
+        }
+        if(isset($record)){
+            $re =Yii::$app->db->createCommand()
+                ->batchInsert('money_trend',['user_id','money','time','status'],
+                    $record)
+                ->execute();
+        }
+
+    }
+
+    public function actionProject(){
+        $finance_projectDb = new Finance_project();
+        $data = $finance_projectDb->find()->where(['status'=>'1'])->asArray()->all();
+        print_r($data);
+    }
+
+
+}
---
+++ home/resources/views/layouts/nav.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="hui/iconfont.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="nav">
+    <ul>
+        <li >
+            <a href="">
+                <span><i class="Hui-iconfont">&#xe67f;</i></span>
+                <span>首页</span>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <span><i class="Hui-iconfont">&#xe627;</i></span>
+                <span>理财</span>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <span><i class="Hui-iconfont this">&#xe60d;</i></span>
+                <span>我的</span>
+            </a>
+        </li>
+    </ul>
+</div>
+</body>
+</html>
