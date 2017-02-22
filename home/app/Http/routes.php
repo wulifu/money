3358df46e678ddacf3864b591031668fe1a4faf5
kangning <kangning_it@163.com>
2017-02-22 09:33:15 +0800

    解决冲突

--- home/app/Http/routes.php
+++ home/app/Http/routes.php
@@ -25,7 +25,6 @@ Route::get('/', function () {
 | kernel and includes session state, CSRF protection, and more.
 |
 */
-<<<<<<< HEAD
 
 Route::group(['middleware' => ['web']], function () {
     //
@@ -38,74 +37,65 @@ Route::get('account','AccountController@index');
 Route::any('index','IndexController@index');
 
 
-=======
->>>>>>> 02931e66f1764cd004518ec0001488664605a7c1
 Route::any('register','RegisterController@index');//注册页面一
 Route::any('registers','RegisterController@register'); //注册页面二
 
 Route::group(['middleware' => ['web','common']], function () {
 	
 
-Route::any('checkcode','RegisterController@checkcode');//发送验证码
-Route::any('user_add','RegisterController@add'); //用户添加
-Route::any('login','LoginController@index'); 	//验证用户登录
+    Route::any('checkcode','RegisterController@checkcode');//发送验证码
+    Route::any('user_add','RegisterController@add'); //用户添加
+    Route::any('login','LoginController@index'); 	//验证用户登录
 
-Route::any('backpwd','RegisterController@backpassword'); // 重置密码页面
-Route::any('user_updatepwd','RegisterController@updatepwd'); //用户忘记密码
-Route::any('user_shiming','RegisterController@shiming'); //用户实名认证
-Route::any('user_editpwd','RegisterController@editpwd'); //用户修改密码
-Route::any('user_editpay','RegisterController@editpay'); //用户修改支付密码
-Route::any('user_bankcard','RegisterController@bankcard'); //用户个人中心绑定银行卡
-//投资项目
-Route::get('project','ProjectController@index');
-Route::get('details','ProjectController@details');
-Route::get('payment','ProjectController@payment');
-Route::get('account','AccountController@index');
-
-
-// Route::get('comm','CommonController@index'); //测试
+    Route::any('backpwd','RegisterController@backpassword'); // 重置密码页面
+    Route::any('user_updatepwd','RegisterController@updatepwd'); //用户忘记密码
+    Route::any('user_shiming','RegisterController@shiming'); //用户实名认证
+    Route::any('user_editpwd','RegisterController@editpwd'); //用户修改密码
+    Route::any('user_editpay','RegisterController@editpay'); //用户修改支付密码
+    Route::any('user_bankcard','RegisterController@bankcard'); //用户个人中心绑定银行卡
+    //投资项目
+    Route::get('project','ProjectController@index');
+    Route::get('details','ProjectController@details');
+    Route::get('payment','ProjectController@payment');
+    Route::get('account','AccountController@index');
 
 
-
-Route::get('getProperty','AccountController@getProperty');
-Route::get('getBill','AccountController@getBill');
-
-<<<<<<< HEAD
-Route::get('datum','AccountController@datum');
-
-Route::get('getIsBinding','AccountController@getIsBinding');
-
-Route::get('getIsBinding','AccountController@getIsBinding');
+    // Route::get('comm','CommonController@index'); //测试
 
 
 
-//投资项目
-Route::get('project','ProjectController@index');
-Route::get('details','P.rojectController@details');
+    Route::get('getProperty','AccountController@getProperty');
+    Route::get('getBill','AccountController@getBill');
+
+    Route::get('datum','AccountController@datum');
+
+    Route::get('getIsBinding','AccountController@getIsBinding');
+
+    Route::get('getIsBinding','AccountController@getIsBinding');
 
 
-=======
->>>>>>> 02931e66f1764cd004518ec0001488664605a7c1
 
-Route::get('getIsBinding','AccountController@getIsBinding');
-
-Route::get('getIsBinding','AccountController@getIsBinding');
-<<<<<<< HEAD
-//投资项目
-Route::get('project','ProjectController@index');
-Route::get('details','ProjectController@details');
-Route::get('payment','ProjectController@payment');
-=======
-
-Route::get('recharge','AccountController@recharge');
-Route::get('binding','AccountController@binding');
+    //投资项目
+    Route::get('project','ProjectController@index');
+    Route::get('details','P.rojectController@details');
 
 
-Route::any('index','IndexController@index');
+    Route::get('getIsBinding','AccountController@getIsBinding');
+
+    Route::get('getIsBinding','AccountController@getIsBinding');
+
+    Route::get('project','ProjectController@index');
+    Route::get('details','ProjectController@details');
+    Route::get('payment','ProjectController@payment');
+
+    Route::get('recharge','AccountController@recharge');
+    Route::get('binding','AccountController@binding');
+
+
+    Route::any('index','IndexController@index');
 
 
 });
 
 
->>>>>>> 02931e66f1764cd004518ec0001488664605a7c1
 
--- home/resources/views/layouts/nav.blade.php
+++ home/resources/views/layouts/nav.blade.php
@@ -2,34 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-<<<<<<< HEAD
     <title>Document</title>
-
-    <link href="{{asset('hui/iconfont.css')}}" rel="stylesheet" type="text/css" />
-
     <link href="hui/iconfont.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="css/css.css">
 
-
-    <link href="{{asset('css/css.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('hui/iconfont.css')}}" rel="stylesheet" type="text/css" />
-
-=======
-    <title>Document</title> 
-    <link href="hui/iconfont.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="css/css.css">
-
->>>>>>> 02931e66f1764cd004518ec0001488664605a7c1
 </head>
 <body>
 <div class="nav">
     <ul>
         <li >
-<<<<<<< HEAD
-            <a href="index">
-=======
             <a href="/index">
->>>>>>> 02931e66f1764cd004518ec0001488664605a7c1
                 <span><i class="Hui-iconfont">&#xe67f;</i></span>
                 <span>首页</span>
             </a>
@@ -41,19 +23,9 @@
             </a>
         </li>
         <li>
-<<<<<<< HEAD
-            <a href="account">
-
-                <span><i class="Hui-iconfont ">&#xe60d;</i></span>
-                <span>我的</span>
-
-               
-
-=======
             <a href="/account">
                 <span><i class="Hui-iconfont ">&#xe60d;</i></span>
                 <span>我的</span>
->>>>>>> 02931e66f1764cd004518ec0001488664605a7c1
             </a>
         </li>
     </ul>
