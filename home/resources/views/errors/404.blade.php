407ac30e6821d53c9b84af7e948af70fb7223282
xiaoxuesheng <645615159@qq.com>
2017-02-22 13:53:57 +0800

    ss

--- home/resources/views/errors/404.blade.php
+++ home/resources/views/errors/404.blade.php
@@ -1,4 +1,97 @@
-<?php
- header('content-type:text/html;charset=utf-8');
-?>
-<script type="text/javascript" src="http://www.qq.com/404/search_children.js" charset="utf-8" homePageUrl="index" homePageName="回到我的主页"></script>
+<a href="http://www.100sucai.com" style="display:none">http://www.100sucai.com</a>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Poses-404 for Iphone, Android &nbsp; Smartphone Mobile Website Template | Home : w3layouts</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="The free Poses-404 Iphone web template, Andriod web template, Smartphone web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+<div class="wrap">
+	<h1><b><b><b>Every day money</b></b></b></h1>
+	<div class="banner">
+		<img src="./images/banner.png" alt="" />
+	</div>
+	<div class="page">
+		<h2>Dude,we can't find that page!</h2>
+	</div>
+	<div class="footer">
+		<!-- <p>Design by <a href="http://w3layouts.com">w3layouts</a></p> -->
+	</div>
+</div>
+</body>
+</html>
+<style type="text/css">
+	/* reset */
+html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,dl,dt,dd,ol,nav ul,nav li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;}
+article, aside, details, figcaption, figure,footer, header, hgroup, menu, nav, section {display: block;}
+ol,ul{list-style:none;margin:0px;padding:0px;}
+blockquote,q{quotes:none;}
+blockquote:before,blockquote:after,q:before,q:after{content:'';content:none;}
+table{border-collapse:collapse;border-spacing:0;}
+/* start editing from here */
+a{text-decoration:none;}
+.txt-rt{text-align:right;}/* text align right */
+.txt-lt{text-align:left;}/* text align left */
+.txt-center{text-align:center;}/* text align center */
+.float-rt{float:right;}/* float right */
+.float-lt{float:left;}/* float left */
+.clear{clear:both;}/* clear float */
+.pos-relative{position:relative;}/* Position Relative */
+.pos-absolute{position:absolute;}/* Position Absolute */
+.vertical-base{	vertical-align:baseline;}/* vertical align baseline */
+.vertical-top{	vertical-align:top;}/* vertical align top */
+.underline{	padding-bottom:5px;	border-bottom: 1px solid #eee; margin:0 0 20px 0;}/* Add 5px bottom padding and a underline */
+nav.vertical ul li{	display:block;}/* vertical menu */
+nav.horizontal ul li{	display: inline-block;}/* horizontal menu */
+img{max-width:100%;}
+/*end reset*/
+body{
+	font-family: "Century Gothic" Helvetica, sans-serif;
+	background: url('./images/bg.png');
+}
+.wrap{
+	margin:0 auto; 
+	padding: 10px;
+}
+h1{
+	text-align:center;
+	margin-top: 20px;
+	color: #603813;
+	font-size: 2em;
+	text-transform: uppercase;
+	font-weight: bold;
+}
+.banner{
+	text-align:center;
+	margin-top: 20px;
+}
+.page{
+	text-align:center;
+	font-family: "Century Gothic";
+}
+.page h2{
+	font-size:1.8em;
+	color: rgb(99, 44, 37);
+	font-weight:bold;
+}
+.footer{
+	font-family: "Century Gothic";
+	margin:20px 0 10px;
+}
+.footer p{
+	text-align:right;
+	font-size:1em;
+	color: #603813;
+}
+.footer a{
+	 color: #f9614d;
+}
+.footer a:hover{
+	text-decoration:underline;
+}
+
+</style>
+
