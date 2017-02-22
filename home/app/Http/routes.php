eac200e51944624436b4c2ef316286c9d8b09c6a
hezhongzhong <it_ll@sina.com>
2017-02-22 10:12:57 +0800

    和建港

--- home/app/Http/routes.php
+++ home/app/Http/routes.php
@@ -34,41 +34,20 @@ Route::group(['middleware' => ['web']], function () {
 Route::get('account','AccountController@index');
 
 
-
 Route::any('index','IndexController@index');
 
 
 Route::any('register','RegisterController@index');//注册页面一
 Route::any('registers','RegisterController@register'); //注册页面二
-
 Route::any('checkcode','RegisterController@checkcode');//发送验证码
 Route::any('backpwd','RegisterController@backpassword'); // 重置密码页面
 Route::any('user_updatepwd','RegisterController@updatepwd'); //用户忘记密码
 
 
 Route::group(['middleware' => ['web','common']], function () {
-	
 
-Route::any('checkcode','RegisterController@checkcode');//发送验证码
-Route::any('user_add','RegisterController@add'); //用户添加
-Route::any('login','LoginController@index'); 	//验证用户登录
-
-Route::any('backpwd','RegisterController@backpassword'); // 重置密码页面
-Route::any('user_updatepwd','RegisterController@updatepwd'); //用户忘记密码
-Route::any('user_shiming','RegisterController@shiming'); //用户实名认证
-Route::any('user_editpwd','RegisterController@editpwd'); //用户修改密码
-Route::any('user_editpay','RegisterController@editpay'); //用户修改支付密码
-Route::any('user_bankcard','RegisterController@bankcard'); //用户个人中心绑定银行卡
-//投资项目
-Route::get('project','ProjectController@index');
-Route::get('details','ProjectController@details');
-Route::get('payment','ProjectController@payment');
-Route::get('account','AccountController@index');
-
-
-
-	
-	
+	Route::any('user_add','RegisterController@add'); //用户添加
+	Route::any('login','LoginController@index'); 	//验证用户登录
 	Route::any('user_shiming','RegisterController@shiming'); //用户实名认证
 	Route::any('user_editpwd','RegisterController@editpwd'); //用户修改密码
 	Route::any('user_editpay','RegisterController@editpay'); //用户修改支付密码
@@ -78,44 +57,12 @@ Route::get('account','AccountController@index');
 	Route::get('details','ProjectController@details');
 	Route::get('payment','ProjectController@payment');
 	Route::get('account','AccountController@index');
-
-
-
-
-Route::get('getProperty','AccountController@getProperty');
-Route::get('getBill','AccountController@getBill');
-
-
-Route::get('datum','AccountController@datum');
-
-Route::get('getIsBinding','AccountController@getIsBinding');
-
-Route::get('getIsBinding','AccountController@getIsBinding');
-
-
-
-//投资项目
-Route::get('project','ProjectController@index');
-Route::get('details','P.rojectController@details');
-
-
-
-
-Route::get('getIsBinding','AccountController@getIsBinding');
-
-Route::get('getIsBinding','AccountController@getIsBinding');
-
-//投资项目
-Route::get('project','ProjectController@index');
-Route::get('details','ProjectController@details');
-Route::get('payment','ProjectController@payment');
-
-
-Route::get('recharge','AccountController@recharge');
-Route::get('binding','AccountController@binding');
-
-
-Route::any('index','IndexController@index');
+	Route::get('getProperty','AccountController@getProperty');
+	Route::get('getBill','AccountController@getBill');
+	Route::get('datum','AccountController@datum');
+	Route::get('getIsBinding','AccountController@getIsBinding');
+	Route::get('recharge','AccountController@recharge');
+	Route::get('binding','AccountController@binding');
 
 
 });
